ebc8414db465c90ea743371729e4b3160bf1feb5
Marty_276 <martybedoya10@gmail.com>
2025-01-30 17:22:09 -0500

    started page building and styling (two sections done)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+const poppins = Poppins({
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,8 +19,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+    <html lang="es">
+      <body style={{fontFamily: poppins.style.fontFamily}}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,49 @@
+import Image from "next/image"
 
 export default function Home() {
   return (
     <>
-      Hello world
+      <main>
+        <header className="page_header">
+          <Image
+            src="/profile_picture.jpg"
+            alt="Foto de perfil de Tach"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="profile_picture"
+          />
+          <h1>Tach Music</h1>
+        </header>
+
+        <section className="latest_release_section">
+          <Image
+            src="/YTSDTVN_cover.jpg"
+            alt="Portada del EP llamado YTSDTVN"
+            width={0}
+            height={0}
+            sizes="100vw"
+            className="latest_release_image"
+          />
+          <a href=""><p className="latest_release_main_text">
+            Escucha el EP YTSDTVN
+            <br/>
+            Nuestro último lanzamiento
+          </p></a>
+          <p className="latest_release_secondary_text">
+            Disponible en todas las plataformas digitales
+          </p>
+        </section>
+
+        <section className="links">
+          <header>
+            <h3>Links principales:</h3>
+          </header>
+          <ul className="links_list">
+
+          </ul>
+        </section>
+      </main>
     </>
   )
 }
