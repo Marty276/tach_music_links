76d3a5ea8b642a0ccb84b4835167103be9f335f6
Marty_276 <martybedoya10@gmail.com>
2025-01-31 16:15:51 -0500

    worked on the links section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,6 +21,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
       <body style={{fontFamily: poppins.style.fontFamily}}>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,39 @@
 import Image from "next/image"
+import Link from "next/link"
+
+type linkInfo = {
+  icon_src?: string
+  text: string
+  colors: `#${string}`[]
+  url?: string
+}
+
+const main_links: linkInfo[] = [
+  {
+    icon_src: "/Spotify_logo.svg",
+    text: "Spotify",
+    colors: ["#000", "#1ED760"],
+    url: ""
+  },
+  {
+    icon_src: "/Instagram_logo.svg",
+    text: "Instagram",
+    colors: ["#ffdc80", "#e1306c", "#5b51d8"],
+    url: ""
+  },
+  {
+    icon_src: "/Tiktok_logo.svg",
+    text: "TikTok",
+    colors: ["#69C9D0", "#010101", "#EE1D52"],
+    url: ""
+  },
+  {
+    icon_src: "/Youtube_logo.svg",
+    text: "Youtube",
+    colors: ["#FF0000", "#282828"],
+    url: ""
+  }
+]
 
 export default function Home() {
   return (
@@ -27,7 +62,7 @@ export default function Home() {
           />
           <a href=""><p className="latest_release_main_text">
             Escucha el EP YTSDTVN
-            <br/>
+            <br />
             Nuestro último lanzamiento
           </p></a>
           <p className="latest_release_secondary_text">
@@ -35,15 +70,32 @@ export default function Home() {
           </p>
         </section>
 
-        <section className="links">
+        <section className="links_section">
           <header>
             <h3>Links principales:</h3>
           </header>
           <ul className="links_list">
-
+            {main_links.map((linkInfo, id) => <LinkButton key={id} {...linkInfo} />)}
           </ul>
         </section>
       </main>
     </>
   )
+}
+
+function LinkButton({ icon_src, text, colors, url }: linkInfo) {
+  return <Link
+    className="link"
+    style={{ background: `linear-gradient(to right, ${colors.join(", ")})` }}
+    href={`${url}`}
+  >
+    {icon_src && <Image
+      src={icon_src}
+      alt={text}
+      width={0}
+      height={0}
+      sizes="100vw"
+    />}
+    <span>{text}</span>
+  </Link>
 }
